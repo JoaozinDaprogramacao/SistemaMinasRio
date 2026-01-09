0768be1fdadb82d6526c01173a0206a50d14416e
João Emanuel <joaoemanueljanauba@gmail.com>
2026-01-09 12:29:49 -0300

    feat: estrutura básica do comissao vendas

--- sistema/painel/index.php
+++ sistema/painel/index.php
@@ -430,6 +430,7 @@ if (@count($res1) > 0) {
 									<span class="side-menu__label" style="margin-left: 15px">Detalhes Romaneios</span><i class="angle fe fe-chevron-right"></i></a>
 								<ul class="slide-menu">
 									<li class="<?php echo @$produtos ?>"><a class="slide-item" href="romaneio_venda_detalhes"> Romaneio de Vendas</a></li>
+									<li class="<?php echo @$produtos ?>"><a class="slide-item" href="comissao_vendas"> Comissão Vendas</a></li>
 								</ul>
 							</li>
 
---
+++ sistema/painel/paginas/comissao_vendas.php
@@ -0,0 +1,115 @@
+<?php
+$pag = 'romaneio_venda_detalhes';
+
+//verificar se ele tem a permissão de estar nessa página
+if (@$produtos == 'ocultar') {
+	echo "<script>window.location='../index.php'</script>";
+	exit();
+}
+?>
+
+<script src="js/ajax.js"></script>
+
+<div class="justify-content-between">
+	<div class="left-content mt-2 mb-3">
+		<div class="row g-2 mb-3 mt-1 align-items-center">
+			<div class="col-auto">
+				<select name="cliente" id="cliente" class="form-select form-select-sm" onchange="buscar()">
+					<option value="">Cliente</option>
+					<?php
+					$query = $pdo->query("SELECT * FROM clientes ORDER BY id DESC");
+					$res = $query->fetchAll(PDO::FETCH_ASSOC);
+					for ($i = 0; $i < @count($res); $i++) {
+						echo '<option value="' . $res[$i]['id'] . '">' . $res[$i]['nome'] . ' - ' . $res[$i]['cpf'] . '</option>';
+					}
+					?>
+				</select>
+			</div>
+
+			<div class="col-auto">
+				<input type="date" name="dataInicial" id="dataInicial" class="date form-control form-control-sm" onchange="buscar()">
+			</div>
+
+			<div class="col-auto">
+				<input type="date" name="dataFinal" id="dataFinal" class="date form-control form-control-sm" onchange="buscar()">
+			</div>
+
+			<div class="col-auto">
+				<button type="button" class="btn btn-outline-secondary btn-sm" onclick="limparFiltros()" title="Limpar Filtros">
+					<i class="fa fa-eraser"></i> Limpar
+				</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card shadow mb-4" style="border-top: 4px solid #2b7a00;">
+		<div class="card-body" id="listar-resumo">
+		</div>
+	</div>
+
+	<div class="row row-sm">
+		<div class="col-lg-12">
+			<div class="card custom-card">
+				<div class="card-body" id="listar">
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include_once("../painel/paginas/romaneio_venda_detalhes/romaneio_venda_detalhes/modais.php"); ?>
+
+	<input type="hidden" id="ids">
+
+	<script>
+		var pag = "<?= $pag ?>";
+		// Inicializa a lista ao carregar a página
+		window.onload = function() {
+			if (typeof buscar === 'function') buscar();
+		};
+	</script>
+
+	<script src="../painel/paginas/romaneio_venda_detalhes/romaneio_venda_detalhes/romaneio_venda_detalhes_scripts.js"></script>
+
+	<script type="text/javascript">
+		var pag = "<?= $pag ?>"
+	</script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('.select2').select2({
+				placeholder: "Selecione os romaneios",
+				allowClear: true,
+				width: 'resolve'
+			});
+		});
+	</script>
+
+
+	<script type="text/javascript">
+		function carregarImg() {
+			var target = document.getElementById('target');
+			var file = document.querySelector("#foto").files[0];
+
+			var reader = new FileReader();
+
+			reader.onloadend = function() {
+				target.src = reader.result;
+			};
+
+			if (file) {
+				reader.readAsDataURL(file);
+
+			} else {
+				target.src = "";
+			}
+		}
+	</script>
+
+
+	<script type="text/javascript">
+		function buscarCat(id) {
+			$('#cat').val(id);
+			listar(id)
+		}
+	</script>
